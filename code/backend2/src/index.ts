b06c98f983def1c5a9f7d6e51353d085a16abc84
suzal777 <sujalphaiju777@gmail.com>
2025-07-01 00:19:38 +0545

    4th commit

--- code/backend2/src/index.ts
+++ code/backend2/src/index.ts
@@ -26,4 +26,4 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
